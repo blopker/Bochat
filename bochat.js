5ec8dd853256ea67cd248dadd718a5c978674fab
Bo Lopker <blopker@23andme.com>
2015-10-10 18:37:58 -0700

    rebrand

--- bochat.js
+++ bochat.js
@@ -1,17 +1,15 @@
-'use strict';
-
 let Chats = new Mongo.Collection('chats');
 
 function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for( var i=0; i < 5; i++ )
+  for( var i = 0; i < 5; i++ ) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
 
   return text;
 }
-
 
 if (Meteor.isClient) {
 
